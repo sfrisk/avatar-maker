140b4ceca4c3333e6c9b631423bdb98bc57475a3
Sarah Frisk <sarah@sarahfrisk.com>
2021-05-02 12:22:36 -0400

    remove unneeded code

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,15 +7,6 @@ const routes: Array<RouteRecordRaw> = [
     name: "Home",
     component: Home,
   },
-  {
-    path: "/dolphin",
-    name: "Dolphin",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "dolphin" */ "../views/Dolphin.vue"),
-  },
 ];
 
 const router = createRouter({
